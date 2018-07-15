59a8da12cb960ff9ba87f0b95d5474941b9ba830
jillian <a.jillian.graham@gmail.com>
2018-07-15 16:25:56 -0300

    the world is ending

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-	root 'application#hola'
+	root 'application#goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
